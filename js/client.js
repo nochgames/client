12e81be0e6525d588e560a89306990e89c5cfa17
Dmitry Tsyganov <dmitrytsgn@gmail.com>
2017-07-28 00:49:52 +0300

    changed fill style

--- js/client.js
+++ js/client.js
@@ -914,7 +914,7 @@ Noch.prototype = {
     },
 
     drawElement: function(x, y, radius, color) {
-        this.renderingTool.setFillStyle('black');
+        this.renderingTool.setFillStyle('#0b0b0b');
         this.renderingTool.setStrokeColor(color);
         this.renderingTool.drawCircle(x, y, radius);
     },
